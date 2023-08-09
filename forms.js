660789575c2cefb9d664c7f3e4aa82d0825d3fc2
Gabriela Candido <134517775+GabrielaCarolinaCandido@users.noreply.github.com>
2023-08-09 10:56:14 -0300

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,10 @@
+function Enviar(){
+    var nome = document.getElementById("nomeid");
+    if(nome.value !=""){
+        alert("Obrigado sr(a)"+ 
+        "seu formulário foi enviado e descartado") 
+    }
+    else{
+        alert("what is this")
+    }
+}
